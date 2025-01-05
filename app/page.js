8cb0dac34e9c3dc829cfcc4237b3710c2db781f5
Grant Wells <47901670+grantwells10@users.noreply.github.com>
2025-01-05 17:14:02 -0500

    =Header

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 'use client'
 import NavBar from "./components/Navbar"
+import Header from "./components/Header"
 
 export default function Home() {
   return (
    <>
-    <NavBar></NavBar>
+    <NavBar />
+    <Header />
    </>
   );
 }
