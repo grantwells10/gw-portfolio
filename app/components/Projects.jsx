5674f672b4aa89395dbd7f5ce06a9c39ec99e47d
Grant Wells <47901670+grantwells10@users.noreply.github.com>
2025-01-06 17:08:35 -0500

    =Contact form

--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -5,18 +5,16 @@ import Image from 'next/image'
 const Projects = () => {
   return (
     <div id='projects' className='w-full px-[12%] py-10 scroll-mt-20'>
-      <h4 className='text-center mb-2 text-lg font-Ovo'>
+        <h4 className='text-center mb-2 text-lg font-Ovo'>
             My work
         </h4>
         <h2 className='text-center text-5xl font-Ovo'>
             Highlighted programming projects
         </h2>
-
         <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>
             I have worked on several programming including full-stack iOS delevopment,
             machine learning, and more.
         </p>
-
         <div className='grid grid-cols-auto my-10 gap-5'>
             {workData.map(({title, description, bgImage}, idx) => (
                 <div key={idx} 
@@ -39,7 +37,6 @@ const Projects = () => {
                 </div>
             ))}
         </div>
-
         <a href='' className='w-max flex items-center justify-center gap-2 text-gray-700
         border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20
         hover:bg-lightHover duration-500'>
