519a292e5d81dab87b67b47fc0bfde32806271a8
Grant Wells <47901670+grantwells10@users.noreply.github.com>
2025-01-06 15:44:05 -0500

    About section

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 'use client'
 import NavBar from "./components/Navbar"
 import Header from "./components/Header"
+import About from "./components/About"
 
 export default function Home() {
   return (
    <>
     <NavBar />
     <Header />
+    <About />
    </>
   );
 }
