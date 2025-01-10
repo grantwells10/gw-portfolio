9e5abc0521c5e3b51c3704c42224d734c006232c
Grant Wells <47901670+grantwells10@users.noreply.github.com>
2025-01-10 12:33:36 -0500

    Now checks user preferences for the theme

--- app/page.js
+++ app/page.js
@@ -13,13 +13,13 @@ export default function Home() {
   const [isDarkMode, setIsDarkMode] = useState(true);
 
   // check user's preferences
-  // useEffect(() => {
-  //  if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-  //    setIsDarkMode(true);
-  //  } else {
-  //    setIsDarkMode(false);
-  //  }
-  // },[])
+  useEffect(() => {
+  if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    setIsDarkMode(true);
+  } else {
+    setIsDarkMode(false);
+    }
+  },[])
 
   // act based on theme button
   useEffect(() => {
